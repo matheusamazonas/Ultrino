7449074ef3624036e6aa1ae26231852b0ec0f97e
Matheus Andrade <matheus.amazonas@gmail.com>
2018-07-14 16:43:27 +0200

    Getting the distance

--- src/Ultrino.cpp
+++ src/Ultrino.cpp
@@ -7,13 +7,30 @@
 #include <Arduino.h>
 #include "Ultrino.h"
 
+#define US_SPEED 343	// m/s
+
 Ultrino::Ultrino(uint8_t triggerPin, uint8_t pulsePin)
 {
 	_triggerPin = triggerPin;
 	_pulsePin = pulsePin;
+
+	pinMode(_triggerPin, OUTPUT);
+	pinMode(_pulsePin, INPUT);
 }
 
+/**
+	Returns the distance in meters
+*/
 double Ultrino::getDistance()
 {
-	return 0;
+	digitalWrite(_triggerPin, LOW);
+	// The delay here is arbitrary. Just waiting some time to bring it up again
+	delayMicroseconds(3);
+	digitalWrite(_triggerPin, HIGH);
+	// This delay is on HC-SR04's specification
+	delayMicroseconds(10);
+	digitalWrite(_triggerPin, LOW);
+
+	double responseTime = pulseIn(_pulsePin, HIGH); // In microseconds
+	return (responseTime / 1E6) * US_SPEED / 2.0;
 }
